7a037185bcbd9f3ae20ecf3f1ef0e22849793738
Dillon Barnes <dillonb07dev@gmail.com>
2024-04-10 15:02:53 +0100

    ✨ Working websocket server and web clients

---
+++ index.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import type {Request, Response} from 'express';
+import { WebSocketServer } from 'ws';
+
+const app = express();
+const port = 3000;
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+const server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+app.get('/', (_req: Request, res: Response) => {
+    res.render('index.html');
+});
+
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        if (Buffer.isBuffer(message)) {
+            console.log(message.toString())
+        } else {
+            console.log(message, typeof message);
+        }
+        // Send message to all other connections
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === 1) {
+                client.send(Buffer.isBuffer(message) ? message.toString() : message);
+            }
+        });
+    })
+    
+    ws.send("Hi, I'm Bun!");
+});
